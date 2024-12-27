d04da35aaa8beac8e91bf33a0bf1443779903d65
krit-vardhan-mishra <aboywhoisnerdy@gmail.com>
2024-12-27 19:33:46 +0530

    FloatingActionButton

--- app/src/main/java/com/just_for_fun/practicing_android_development/PracticeLoginScreen.kt
+++ app/src/main/java/com/just_for_fun/practicing_android_development/PracticeLoginScreen.kt
@@ -2,6 +2,8 @@ package com.just_for_fun.practicing_android_development
 
 import android.os.Bundle
 import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -10,8 +12,12 @@ import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
 
 class PracticeLoginScreen : AppCompatActivity() {
+
+    private var isRotated = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
 
         setContentView(R.layout.activity_practice_login_screen)
@@ -34,6 +40,27 @@ class PracticeLoginScreen : AppCompatActivity() {
             Snackbar.make(view, "Here's a FloatingActionButton", Snackbar.LENGTH_LONG)
                 .setAction("Action", null)
                 .show()
+
+            val rotateAnimation = AnimationUtils.loadAnimation(this, R.anim.rotate_fab)
+            rotateAnimation.setAnimationListener(object : Animation.AnimationListener {
+                override fun onAnimationStart(animation: Animation?) {}
+
+                override fun onAnimationEnd(animation: Animation?) {
+                    isRotated = !isRotated
+                }
+
+                override fun onAnimationRepeat(p0: Animation?) { }
+            })
+
+            if (isRotated) {
+                val reverseRotateAnimation = AnimationUtils.loadAnimation(this, R.anim.rotate_fab_reverse)
+                fab.startAnimation(reverseRotateAnimation)
+                reverseRotateAnimation.fillAfter = true
+            } else {
+                rotateAnimation.fillAfter = true
+                fab.startAnimation(rotateAnimation)
+            }
         }
+
     }
 }
